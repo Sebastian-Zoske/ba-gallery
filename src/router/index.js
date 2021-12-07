61df88e3cc0ea055544f004d7352cb551552358a
Sebastian <s0554384@htw-berlin.de>
2021-12-07 15:01:07 +0100

    first gallery components - Photo, Gallery, View, Router

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/Home.vue";
+import Photo from "../views/Photo.vue";
 
 const routes = [
   {
     path: "/",
     name: "Home",
     component: Home,
+  },
+  {
+    path: "/photo/:id",
+    name: "home",
+    component: Photo,
   },
   {
     path: "/about",
@@ -19,6 +25,7 @@ const routes = [
 ];
 
 const router = createRouter({
+  mode: "history",
   history: createWebHistory(process.env.BASE_URL),
   routes,
 });
